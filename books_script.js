4bdcc452161a41250e0baf44e806dbb21eb3c496
jonas-45 <jonaswullah@gmail.com>
2023-01-18 07:31:03 +0000

    Refactor javascript code to use classes

--- books_script.js
+++ books_script.js
@@ -1,77 +1,75 @@
+class BooksLibrary{
+  
+  constructor(books,form,nobooks,booksContainer){
+    this.form = form;
+    this.noBooks = nobooks
+    this.booksContainer = booksContainer
+    this.books = books;
+  }
+
+
+  // getAllBooks(){
+  //   return JSON.parse(localStorage.getItem("books")) || [];
+  // }
+
+  showBook(book){
+    this.noBooks.style.display = "none";
+    this.booksContainer.innerHTML += `<div class="single-book">
+    <p class="book-title">${book.title}</p>
+    <p class="book-author">${book.author}</p>
+    <button type="button" class="remove-button">Remove</button>
+    <hr>
+    </div>`
+
+    let removeButtons = document.querySelectorAll(".remove-button");
+    removeButtons.forEach((button,index) => {
+      button.addEventListener("click", () => {
+        this.removeBook(index);
+        
+      });
+    }); 
+  }
+
+  getBooks(){
+    if(this.books.length > 0){
+      this.noBooks.style.display = "none";
+      this.books.forEach((book) => {
+        this.showBook(book);
+      })
+    }
+  }
+
+  addBook(event){
+    event.preventDefault();
+    let newBook = {
+      title: document.getElementById("booktitle").value, 
+      author: document.getElementById("bookauthor").value
+    };
+
+    this.books.push(newBook);
+    form.reset();
+    this.showBook(newBook); 
+    localStorage.setItem("books",JSON.stringify(this.books));
+  }
+
+  removeBook(index){
+    this.books.splice(index,1);
+    localStorage.setItem("books",JSON.stringify(this.books));
+    let bookToRemove = document.querySelectorAll(".single-book");
+    let booksDivs = Array.from(bookToRemove);
+    booksDivs[index].style.display = "none";
+
+    if(this.books.length < 1){
+      this.noBooks.style.display = "block";
+    }
+  }
+}
+
+let books = JSON.parse(localStorage.getItem("books")) || [];
 const form = document.getElementById("form");
 const noBooks = document.querySelector(".no-books");
-let booksContainer = document.querySelector(".books-container");
-let books = JSON.parse(localStorage.getItem("books")) || [];
+const booksContainer = document.querySelector(".books-container");
 
-function showBook(book){
-  noBooks.style.display = "none";
-  booksContainer.innerHTML += `<div class="single-book">
-  <p class="book-title">${book.title}</p>
-  <p class="book-author">${book.author}</p>
-  <button type="button" class="remove-button">Remove</button>
-  <hr>
-</div>`
-
-AddRemoveListerToButtons();
-
-}
-
-function getBooks(){
-
-  if(books.length > 0){
-    noBooks.style.display = "none";
-    books.forEach((book) => {
-      showBook(book);
-    })
-  }
-}
-
-function addBook(event){
-  event.preventDefault();
-
-  let newBook = {
-    title: document.getElementById("booktitle").value, 
-    author: document.getElementById("bookauthor").value
-  };
-
-  books.push(newBook);
-  form.reset();
-  showBook(newBook); 
-  localStorage.setItem("books",JSON.stringify(books));
-
-}
-
-function AddRemoveListerToButtons(){
-  let removeButtons = document.querySelectorAll(".remove-button");
-  removeButtons.forEach((button,index) => {
-    button.addEventListener("click", () => {
-      //removeBook(index)
-      books.splice(index,1);
-      localStorage.setItem("books",JSON.stringify(books));
-      let bookToRemove = document.querySelectorAll(".single-book");
-      let booksDivs = Array.from(bookToRemove);
-      booksDivs[index].style.display = "none";
-
-      if(books.length < 1){
-        noBooks.style.display = "block";
-      }
-    });
-  });
-}
-
-function removeBook(index){
-  books.splice(index,1);
-  localStorage.setItem("books",JSON.stringify(books));
-  let bookToRemove = document.querySelectorAll(".single-book");
-  let booksDivs = Array.from(bookToRemove);
-  booksDivs[index].style.display = "none";
-  
-  //books.length < 1 ? noBooks.style.display = "block" : noBooks.style.display = "none";
-  if(books.length < 1){
-    noBooks.style.display = "block";
-  }
-}
-
-getBooks(books);
-
-form.addEventListener('submit',addBook);
+const booksObj = new BooksLibrary(books,form,noBooks,booksContainer);
+booksObj.getBooks();
+form.addEventListener('submit',(event) => {booksObj.addBook(event)});
